8470b19f2169950425a7b80150cecfb6a927c942
长言 <152174898+nagyan78@users.noreply.github.com>
2025-05-20 12:09:11 +0800

    Update 1.cpp注释

--- 张信菲-20232241014-软件2306-os-hw2-c-example/1.cpp
+++ 张信菲-20232241014-软件2306-os-hw2-c-example/1.cpp
@@ -1,12 +1,12 @@
-#include<stdio.h>
-#include<sys/types.h>
-#include<unistd.h>
+#include<stdio.h> 
+#include<sys/types.h> 
+#include<unistd.h> 
 
 int main()
 {
-    pid_t my_pid;
-    my_pid = getpid();
-    printf("My process ID is %d\n", my_pid);
+    pid_t my_pid; // 定义一个pid_t类型的变量，用于存储进程ID
+    my_pid = getpid(); // 调用getpid()系统调用，获取当前进程的ID，并将其赋值给my_pid
+    printf("My process ID is %d\n", my_pid); // 使用printf函数输出当前进程的ID
 
-    return 0;
+    return 0; 
 }
