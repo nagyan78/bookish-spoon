e521e193fa322fb2c205b1b045f05befd3983dd8
长言 <152174898+nagyan78@users.noreply.github.com>
2025-05-20 12:12:13 +0800

    Update 4.cpp注释

--- 张信菲-20232241014-软件2306-os-hw2-c-example/4.cpp
+++ 张信菲-20232241014-软件2306-os-hw2-c-example/4.cpp
@@ -1,12 +1,12 @@
-#include<stdio.h>
-#include<sys/types.h>
-#include<unistd.h>
+#include<stdio.h> 
+#include<sys/types.h> 
+#include<unistd.h> 
 
 int main()
 {
-    fork();
-    fork();
-    fork();
-    printf("Hello\n");
-    return 0;
+    fork(); // 第一次调用fork，创建一个子进程
+    fork(); // 第二次调用fork，每个现有进程都会再创建一个子进程
+    fork(); // 第三次调用fork，每个现有进程都会再创建一个子进程
+    printf("Hello\n"); // 所有进程都会执行到这里，并打印"Hello"
+    return 0; 
 }
